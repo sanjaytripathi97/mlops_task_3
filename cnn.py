6b86fc5fd5c1d22f410904400a11f458cbcb086c
Sanjay <Tripathi>
2020-05-26 16:04:40 +0530

    updated2

--- cnn.py
+++ cnn.py
@@ -87,12 +87,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        'cnn_dataset/training_set/',
+        '/root/task3/cnn_dataset/training_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'cnn_dataset/test_set/',
+        '/root/task3/cnn_dataset/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
